e0651e11e309c501b2dff40bfa75f863b6cf00b2
John MOHADDES <johnmohaddes@gmail.com>
2017-07-11 02:19:40 -0700

    Roll btn active player toggling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var scores, roundscores, activePlayer, dice;
 
 scores = [0,0];
-roundscore= 0;
+roundScore= 0;
 activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
@@ -19,8 +19,6 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
-
-
 document.querySelector('.btn-roll').addEventListener('click', function () {
     
     // 1. Random number
@@ -31,7 +29,63 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDOM.style.display = 'block';
     diceDOM.src = 'images/dice-' + dice + '.png';
     
+    
     // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        
+        // Add score
+        roundScore += dice;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+
+    }
+        else {
+            // Next player
+            // ternary opertor
+            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+            roundScore = 0;
+            
+            if (activePlayer === 0) {
+                document.querySelector('.player-1-panel').classList.remove('active');
+                document.querySelector('.player-0-panel').classList.add('active');
+            }
+            
+            else {
+                document.querySelector('.player-0-panel').classList.remove('active');
+                document.querySelector('.player-1-panel').classList.add('active');
+
+                
+            }
+
+        }
     
     
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
