6ea141b5e2976226e3262d0bed835acdf758fe59
John MOHADDES <johnmohaddes@gmail.com>
2017-07-11 01:26:12 -0700

    Roll dice image dix

--- app.js
+++ app.js
@@ -13,3 +13,25 @@ document.getElementById("current-" + activePlayer).textContent = dice;
 var x = document.querySelector("#score-0").textContent;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log('dice');
+    // 2. Display the results
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'images/dice-' + dice + '.png';
+    
+    // 3. Update the round score IF the rolled number was NOT a 1
+    
+    
+})
