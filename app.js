31abe712555af9aff228c2ee735d9d16049b63e6
John MOHADDES <johnmohaddes@gmail.com>
2017-07-11 00:58:47 -0700

    First DOM access & manipulation

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello World!")
+var scores, roundscores, activePlayer, dice;
+
+scores = [0,0];
+roundscore= 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+document.getElementById("current-" + activePlayer).textContent = dice;
+// document.getElementById("current-" + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector("#score-0").textContent;
+
+document.querySelector('.dice').style.display = 'none';
